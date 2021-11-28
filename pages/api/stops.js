c6d741ac967b42d6183c96c5220425fd61f08212
thekhairul <thekhairul.office@gmail.com>
2021-11-28 23:48:37 +0600

    implement calling apis

--- pages/api/stops.js
+++ pages/api/stops.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
     currentRoute.stops.forEach(({stop_en, stop_bn}) => {
       if(!stopMap.has(stop_en)) {
         stopMap.set(stop_en, true);
-        result.push({stop_en, stop_bn});
+        result.push({label: stop_en, value: `${stop_en}|${stop_bn}`});
       }
     })
     return result;
