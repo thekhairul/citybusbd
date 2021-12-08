101395ad6c9827c60d9a98676336975c7afcff3c
thekhairul <thekhairul.office@gmail.com>
2021-12-08 16:53:51 +0600

    add ui to show buses

--- components/Results.js
+++ components/Results.js
@@ -5,7 +5,7 @@ import { RiArrowLeftRightLine } from 'react-icons/ri';
 
 function Results({from, to, matchedRoutes}) {
     return (
-        <Accordion my={10} bg='white' rounded='lg' boxShadow='lg' allowMultiple>
+        <Accordion my={10} bg='white' rounded='lg' boxShadow='lg' defaultIndex={[0]} allowMultiple>
             {
                 matchedRoutes.map(route => {
                     return (<AccordionItem key={route.route}>
@@ -31,16 +31,30 @@ function Results({from, to, matchedRoutes}) {
                             </AccordionButton>
                         </h2>
                         <AccordionPanel pb={4}>
-                            <Box display='flex' flexWrap='wrap'>
-                                {route.stops.map((stop,idx) => {
-                                    return (
-                                        <Box display='flex' alignItems='center' key={idx}>
-                                            <Text p={2} mt={1} mb={1} bg='gray.100' rounded='md' fontSize='sm' outline={stop === from || stop === to ? '2px solid' : 'none'}>{stop}</Text>
-                                            {idx === route.stops.length - 1 ? null : <Icon as={RiArrowLeftRightLine} m={2}/>}
-                                        </Box>
-                                    )
-                                })}
-                            </Box>
+                            <fieldset className='citybus-fieldset'>
+                                <legend className='citybus-legend'>Bus</legend>
+                                <Box display='flex' flexWrap='wrap'>
+                                    {route.buses.map((bus,idx) => {
+                                        return (
+                                            <Text key={idx} p={2} mt={1} mb={1} mr={1} bg='purple.500' color='white' rounded='md' fontSize='sm'>{bus}</Text>
+                                        )
+                                    })}
+                                </Box>
+                                {!route.buses.length ? <Text>No bus found for this route !</Text> : null}
+                            </fieldset>
+                            <fieldset className='citybus-fieldset'>
+                                <legend className='citybus-legend'>Stops</legend>
+                                <Box display='flex' flexWrap='wrap'>
+                                    {route.stops.map((stop,idx) => {
+                                        return (
+                                            <Box display='flex' alignItems='center' key={idx}>
+                                                <Text p={2} mt={1} mb={1} bg='gray.100' rounded='md' fontSize='sm' outline={stop === from || stop === to ? '2px dashed var(--chakra-colors-gray-500)' : 'none'}>{stop}</Text>
+                                                {idx === route.stops.length - 1 ? null : <Icon as={RiArrowLeftRightLine} m={2}/>}
+                                            </Box>
+                                        )
+                                    })}
+                                </Box>
+                            </fieldset>
                         </AccordionPanel>
                     </AccordionItem>);
                 })
